1b18ae768d64a99d06b1cd298c5036f6b3d7667d
Jenny Nilsson <jenny.nilsson@jayway.com>
2013-12-03 09:41:32 +0100

    Updated project setup

--- src/com/jayway/amazon/StartActivity.java
+++ src/com/jayway/amazon/StartActivity.java
@@ -3,12 +3,10 @@ package com.jayway.amazon;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class StartActivity extends Activity
-{
-    /** Called when the activity is first created. */
+public class StartActivity extends Activity {
+
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
